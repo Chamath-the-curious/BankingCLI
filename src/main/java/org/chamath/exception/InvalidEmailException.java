26a8b0e3d1d8bc880e920c7c9455e3bab953de1e
chamath-the-curious <sasmithachamath@gmail.com>
2026-09-12 15:02:40 +0530

    feat: customer creation

---
+++ src/main/java/org/chamath/exception/InvalidEmailException.java
@@ -0,0 +1,9 @@
+package org.chamath.exception;
+
+public class InvalidEmailException extends Exception{
+
+    @Override
+    public String getMessage() {
+        return "Invalid email";
+    }
+}
---
+++ src/main/java/org/chamath/model/Customer.java
@@ -0,0 +1,54 @@
+package org.chamath.model;
+
+import org.apache.commons.validator.routines.EmailValidator;
+import org.chamath.exception.InvalidEmailException;
+import org.chamath.util.CustomerIdGenerator;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Customer {
+    private final long customerId;
+    private String name;
+    private String email;
+    private String phone;
+
+    private static final AtomicLong counter = new AtomicLong(0);
+
+    public Customer(String name, String email, String phone) {
+        this.customerId = CustomerIdGenerator.generateNextId();
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) throws InvalidEmailException {
+        if (EmailValidator.getInstance().isValid(email)) {
+            this.email = email;
+        } else {
+            throw new InvalidEmailException();
+        }
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
