c2f586b8b9280f4cb80fd8507a22d8ce84f71810
chamath-the-curious <sasmithachamath@gmail.com>
2026-09-12 17:51:43 +0530

    tests for savings account deposit and withdraw

---
+++ src/test/java/org/chamath/model/SavingsAccountTest.java
@@ -0,0 +1,61 @@
+package org.chamath.model;
+
+import org.chamath.exception.InsufficientFundsException;
+import org.chamath.exception.NegativeValueException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class SavingsAccountTest {
+
+    @Mock
+    private Customer customer;
+
+    private SavingsAccount savingsAccount;
+
+    @BeforeEach
+    void setup() {
+        savingsAccount = new SavingsAccount(customer, 1000);
+    }
+
+    @Test
+    void depositTest() throws NegativeValueException {
+        savingsAccount.deposit(BigDecimal.valueOf(500));
+        assertEquals(savingsAccount.getBalance(), BigDecimal.valueOf(1500.0));
+    }
+
+    @Test
+    void depositNegativeAmountTest() {
+        assertThrows(
+                NegativeValueException.class,
+                () -> savingsAccount.deposit(BigDecimal.valueOf(-1000)));
+    }
+
+    @Test
+    void withdrawTest() throws NegativeValueException, InsufficientFundsException {
+        savingsAccount.withdraw(BigDecimal.valueOf(500));
+        assertEquals(savingsAccount.getBalance(), BigDecimal.valueOf(500.0));
+    }
+
+    @Test
+    void withdrawNegativeAmountTest() {
+        assertThrows(
+                NegativeValueException.class,
+                () -> savingsAccount.withdraw(BigDecimal.valueOf(-500)));
+    }
+
+    @Test
+    void withdrawAmountHigherThanBalance() {
+        assertThrows(
+                InsufficientFundsException.class,
+                () -> savingsAccount.withdraw(BigDecimal.valueOf(2000)));
+    }
+}
