60fd2ad7f2fa81a0af7bf19b6501479dd2f51ec8
chamath-the-curious <sasmithachamath@gmail.com>
2026-09-13 21:56:33 +0530

    refactor the constructor and implement the rest of methods

--- src/main/java/org/chamath/banking/service/AccountServiceImpl.java
+++ src/main/java/org/chamath/banking/service/AccountServiceImpl.java
@@ -1,19 +1,23 @@
 package org.chamath.banking.service;
 
+import org.chamath.banking.exception.AccountNotFoundException;
+import org.chamath.banking.exception.InsufficientFundsException;
 import org.chamath.banking.exception.NegativeValueException;
 import org.chamath.banking.model.Account;
 import org.chamath.banking.model.CurrentAccount;
 import org.chamath.banking.model.Customer;
 import org.chamath.banking.model.SavingsAccount;
+import org.chamath.banking.repository.AccountRepository;
 import org.chamath.banking.repository.AccountRepositoryImpl;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 
 public class AccountServiceImpl implements AccountService {
-    private AccountRepositoryImpl accountRepository;
+    private AccountRepository accountRepository;
 
-    public AccountServiceImpl() {
-        accountRepository = new AccountRepositoryImpl();
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
     }
 
     @Override
@@ -37,27 +41,64 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public Account viewAccount(String accountNumber) {
-        return accountRepository.getAccountByAccountNumber(accountNumber);
+    public void viewAccount(String accountNumber) {
+        try {
+            Account account = accountRepository.getAccountByAccountNumber(accountNumber);
+            System.out.println(account);
+        } catch (AccountNotFoundException e) {
+            e.getMessage();
+        }
     }
 
     @Override
-    public HashMap<String, Account> viewAllAccounts() {
-        return accountRepository.getAllAccounts();
+    public void viewAllAccounts() {
+        HashMap<String, Account> accountHashMap = accountRepository.getAllAccounts();
+        accountHashMap.values()
+                .forEach(System.out::println);
     }
 
     @Override
     public void depositAccount(String accountNumber, double amount) {
-
+        try {
+            Account account = accountRepository.getAccountByAccountNumber(accountNumber);
+            account.deposit(BigDecimal.valueOf(amount));
+            accountRepository.saveAccount(account);
+        } catch (AccountNotFoundException e) {
+            e.getMessage();
+        } catch (NegativeValueException e) {
+            e.getMessage();
+        }
     }
 
     @Override
     public void withdraw(String accountNumber, double amount) {
-
+        try {
+            Account account = accountRepository.getAccountByAccountNumber(accountNumber);
+            account.withdraw(BigDecimal.valueOf(amount));
+            accountRepository.saveAccount(account);
+        } catch (AccountNotFoundException e) {
+            e.getMessage();
+        } catch (NegativeValueException e) {
+            e.getMessage();
+        } catch (InsufficientFundsException e) {
+            e.getMessage();
+        }
     }
 
     @Override
     public void transfer(String transferFromNumber, String transferToNumber, double amount) {
-
+        try {
+            Account transferFromAccount = accountRepository.getAccountByAccountNumber(transferFromNumber);
+            Account transferToAccount = accountRepository.getAccountByAccountNumber(transferToNumber);
+            transferFromAccount.transfer(transferToAccount, BigDecimal.valueOf(amount));
+            accountRepository.saveAccount(transferFromAccount);
+            accountRepository.saveAccount(transferToAccount);
+        } catch (AccountNotFoundException e) {
+            e.getMessage();
+        } catch (NegativeValueException e) {
+            e.getMessage();
+        } catch (InsufficientFundsException e) {
+            e.getMessage();
+        }
     }
 }
