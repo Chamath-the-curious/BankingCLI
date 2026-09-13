c47d4b75a2cb2436977ad51de365f6bcd4d11d2d
chamath-the-curious <sasmithachamath@gmail.com>
2026-09-13 23:33:21 +0530

    add abstract method to add transaction

--- src/main/java/org/chamath/banking/model/Account.java
+++ src/main/java/org/chamath/banking/model/Account.java
@@ -11,4 +11,5 @@ public interface Account {
     public void transfer(Account transferTo, BigDecimal amount) throws NegativeValueException, InsufficientFundsException;
     public BigDecimal getBalance();
     public String getAccountNumber();
+    public void addTransaction(TransactionType type, double amount);
 }
